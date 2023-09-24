9add6243e6c6abdeb2000eb937fc33087945d1e8
tapiocaboy <kalanamith@outlook.com>
2023-09-25 12:17:12 +1300

    Added unified formatting

--- crates/l1x-sdk/src/store/lookup_map/impls.rs
+++ crates/l1x-sdk/src/store/lookup_map/impls.rs
@@ -6,30 +6,29 @@ use super::LookupMap;
 
 impl<K, V> Extend<(K, V)> for LookupMap<K, V>
 where
-    K: BorshSerialize + Ord,
-    V: BorshSerialize + BorshDeserialize,
+	K: BorshSerialize + Ord,
+	V: BorshSerialize + BorshDeserialize,
 {
-    fn extend<I>(&mut self, iter: I)
-    where
-        I: IntoIterator<Item = (K, V)>,
-    {
-        for (key, value) in iter {
-            self.set(key, Some(value))
-        }
-    }
+	fn extend<I>(&mut self, iter: I)
+	where
+		I: IntoIterator<Item = (K, V)>,
+	{
+		for (key, value) in iter {
+			self.set(key, Some(value))
+		}
+	}
 }
 
 impl<K, V, Q: ?Sized> core::ops::Index<&Q> for LookupMap<K, V>
 where
-    K: BorshSerialize + Ord + Borrow<Q>,
-    V: BorshSerialize + BorshDeserialize,
+	K: BorshSerialize + Ord + Borrow<Q>,
+	V: BorshSerialize + BorshDeserialize,
 
-    Q: BorshSerialize + ToOwned<Owned = K>,
+	Q: BorshSerialize + ToOwned<Owned = K>,
 {
-    type Output = V;
+	type Output = V;
 
-    fn index(&self, index: &Q) -> &Self::Output {
-        self.get(index)
-            .unwrap_or_else(|| crate::panic("does not exist"))
-    }
+	fn index(&self, index: &Q) -> &Self::Output {
+		self.get(index).unwrap_or_else(|| crate::panic("does not exist"))
+	}
 }
